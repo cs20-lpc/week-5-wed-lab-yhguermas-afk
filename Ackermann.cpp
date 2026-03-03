7943396fadb3d65dcc54d100aa8c6e138461662e
Yanni Guermas <yanniguermas@Yannis-MacBook-Air.local>
2026-03-02 22:49:41 -0800

    Ackermann done

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+    if (m == 0) {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    }
+    else { // m > 0 and n > 0
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
 int main() {
@@ -11,7 +19,7 @@ int main() {
     cout << "Enter m and n: ";
     cin >> m >> n;
 
-    cout << "Ackermann(" << m << ", " << n << ") = " 
+    cout << "Ackermann(" << m << ", " << n << ") = "
          << ackermann(m, n) << endl;
 
     return 0;
